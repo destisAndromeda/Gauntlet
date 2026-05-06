9a987243cd4fe618e5d22efa85052385f5c7a632
None <destis_Andromeda@proton.me>
2026-05-06 19:58:49 +0200

    refactor(seeds): delete comments

--- programs/competition-constructor-program/src/seeds.rs
+++ programs/competition-constructor-program/src/seeds.rs
@@ -5,6 +5,3 @@ pub const SEED_COMPETITION:    &[u8] = b"competition";
 pub const SEED_VAULT:          &[u8] = b"vault";
 pub const SEED_PARTICIPANT:    &[u8] = b"participant";
 pub const SEED_LEADER_BOARD:   &[u8] = b"leaderboard";
-// ... Next seeds can be added in competition branches
-// SEED_SWISS_SYSTEM
-// ROUND_ROBIN etc.
